663bbdbfae28c22801ca6f9a8638b6f7fb614eda
Anup Gupta <anuup22.cs@gmail.com>
2025-01-15 18:05:26 +0530

    feat: implement Laptop class and update Dev component for dependency injection

--- src/main/java/com/demo/demo_spring_app/Dev.java
+++ src/main/java/com/demo/demo_spring_app/Dev.java
@@ -1,10 +1,31 @@
 package com.demo.demo_spring_app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Dev {
+
+  // Laptop laptop; Error: null pointer exception
+
+  // @Autowired DI-1: Field Injection
+  Laptop laptop;
+
+  // DI-2: Constructor Injection
+  // @Autowired Optional(Default)
+  // public Dev(Laptop laptop) {
+  // this.laptop = laptop;
+  // }
+
+  // DI-3: Setter Injection
+  @Autowired
+  public void setLaptop(Laptop laptop) {
+    this.laptop = laptop;
+  }
+
   public void build() {
+
+    laptop.compile();
     System.out.println("Building the project");
   }
 }
---
+++ src/main/java/com/demo/demo_spring_app/Laptop.java
@@ -0,0 +1,7 @@
+package com.demo.demo_spring_app;
+
+public class Laptop {
+  public void compile() {
+    System.out.println("Compiling the code");
+  }
+}
